c4bd48a6fb12f89c9333ab86d6968e6de5316a6b
agustin <agustin.macazzaga@codice.dev>
2024-01-08 14:54:59 -0300

    indentation

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 //DEPENDENCY INYECTION
 
-@Injectable() // este inyectable nos permite marcar el CatService como provider, inyectable en un decorador
+@Injectable() // este inyectable nos permite marcar el CatService como provider, inyectable es un decorador
 export class CatService {
   getHello(): string {
     return 'Hello Cats!!!';
