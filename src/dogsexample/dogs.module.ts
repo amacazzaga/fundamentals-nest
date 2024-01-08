4fc23e280b11fd1d43a1f2f433e159024ad92dab
agustin <agustin.macazzaga@codice.dev>
2024-01-08 17:11:24 -0300

    comments on module

--- src/dogsexample/dogs.module.ts
+++ src/dogsexample/dogs.module.ts
@@ -2,26 +2,22 @@ import { Module } from '@nestjs/common';
 import { DogService } from './dogsservice';
 import { DogController } from './dogscontroller';
 
-
 // simulamos tener un servicio aca, mas tarde debera estar en el dogsservice
 class ExampleServiceOfDogs {
     constructor(private readonly options: string) {}
-  
     getHello(): string {
       return `Hello from ExampleService of the dogs  with options: ${this.options}`;
     }
   }
- //
- // nuestro provider recupera el servicio de dogs en la linea 17
+ // nuestro provider recupera el servicio de dogs provide: exampleserviceofdogs
   const exampleProvider = {
     provide: ExampleServiceOfDogs,
-    useFactory: (options: string) => {
+    useFactory: (options: string) => { // cumplir con el mandatory que dice la docu 
       return new ExampleServiceOfDogs(options);
     },
-    inject: ['EXAMPLE_OPTIONS'], //Las dependencias necesarias para la creación de esta instancia se especifican en el array inject
+    inject: [""], //Las dependencias necesarias para la creación de esta instancia se especifican en el array inject
   };
   //
-
 @Module({
   imports: [],
   controllers: [DogController],
