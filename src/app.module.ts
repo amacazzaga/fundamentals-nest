cd5add982b513b8ce3f0cc4121b8a874e6618a43
agustin <agustin.macazzaga@codice.dev>
2024-01-08 14:37:44 -0300

    useclass example on module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,3 +8,34 @@ import { CatService } from './app.service';
   providers: [CatService], // asociamos el token catservice a la clase CatService segun la documentacion
 })
 export class AppModule {}
+
+/* si quisieramos usar un objeto de mock en lugar , este es un ejemplo pensado para hacer un test!
+const mockCatsService = {
+   mock implementacion aca
+  ...
+};
+@Module({
+  imports: [CatsModule], // esto importa el modulo de Cat entero!! este es el modulo general
+  providers: [
+    {
+      provide: CatsService,
+      useValue: mockCatsService, // aca usamos el useValue que es usar CatService para testing
+    },
+  ],
+})
+export class AppModule {}
+*/
+/*
+si quisieramos usar dinamicamente una clase , debemos usar useClass
+const configServiceProvider = {
+  provide: ConfigService,
+  useClass:
+    process.env.NODE_ENV === 'development'
+      ? DevelopmentConfigService
+      : ProductionConfigService,
+};
+@Module({
+  providers: [configServiceProvider], // a este mismo provider podriamos enviarle CatService y nos quedaria algo asi [configServiceProvider,CatService]
+})
+export class AppModule {}
+*/
